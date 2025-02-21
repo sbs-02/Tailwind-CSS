5786dae5916b23fc18ab46b8821a8a0b1698f52e
sbs002-hub <dangolsaksham8@gmail.com>
2025-02-21 16:58:27 +0545

    initial git

--- Shopify/tailwind.config.js
+++ Shopify/tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./dist/**/*.html"],
   theme: {
+    screens: {
+      sm: "768px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "1024px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1280px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1536px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1800px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
